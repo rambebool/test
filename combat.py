807f5acfd1fc3c66d69145a1e3763754dc71f898
rambebool <rambebool@gmail.com>
2026-06-09 22:37:54 +1000

    update

--- combat.py
+++ combat.py
@@ -21,22 +21,27 @@ def attack_hero(attacker, defender):
         critical_damage = random.randint(20, 30) 
 
         if (critical_damage >= 26):
-            print(f"ВОУ! Прямое попадание в головёшку игрока {defender['name']} в размере {critical_damage}")
+            print(f"[bold red][КРИТИЧЕСКИЙ УРОН][/] Прямое попадание в головёшку игроку {defender['name']} [bold red](урон: {critical_damage})[/]")
             defender["hp"] = defender["hp"] - critical_damage
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{defender['name']}: {defender['hp']}[/]")
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{attacker['name']}: {attacker['hp']}[/]")
         elif (critical_damage <= 25):
-            print(f"{defender['name']} получил леща в размере {critical_damage}")
+            print(f"[bold red][КРИТИЧЕСКИЙ УРОН][/] Игрок {defender['name']} получил леща! [bold red](урон: {critical_damage})[/]")
             defender["hp"] = defender["hp"] - critical_damage
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{defender['name']}: {defender['hp']}[/]")
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{attacker['name']}: {attacker['hp']}[/]")
     if (critical_damage_flag == 0):
-        print(f"\tКритического удара не случилось.")
-        defender["hp"] = defender["hp"] - attacker["damage"]
-        print(f"{defender['name']} ПОЛУЧИЛ УРОН В РАЗМЕРЕ: {attacker['damage']}")
-        print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{defender['name']}: {defender['hp']}[/]")
-        print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{attacker['name']}: {attacker['hp']}[/]")
-
+        shield_damage_flag = random.randint(0, 1)
+        if (shield_damage_flag == 0):
+            print(f"[bold yellow][БЛОК][/] [bold red]{defender['name']} не смог заблокировать урон![/]")
+            defender["hp"] = defender["hp"] - attacker["damage"]
+            print(f"{defender['name']} ПОЛУЧИЛ УРОН В РАЗМЕРЕ: {attacker['damage']}")
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{defender['name']}: {defender['hp']}[/]")
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{attacker['name']}: {attacker['hp']}[/]")
+        elif (shield_damage_flag == 1):
+            print(f"[bold green][БЛОК] [/][bold orange3]{defender['name']} заблокировал урон в размере[/] [bold red]{attacker['damage']}![/]")
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{defender['name']}: {defender['hp']}[/]")
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{attacker['name']}: {attacker['hp']}[/]")
     return
 
 def who_lose(hero_1, hero_2):
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from rich import print
 from rich.panel import Panel
@@ -16,14 +17,28 @@ print(Panel(f"ИМЯ: {attacker['name']}, ХП: {attacker['hp']}, ДАМАГ: {a
 print(Panel(f"ИМЯ: {defender['name']}, ХП: {defender['hp']}, ДАМАГ: {defender['damage']}, УРОВЕНЬ: {defender['level']}", title="Герой 2"))
 
 while(attacker["hp"] > 0 and defender["hp"] > 0):
-    print(f"\n\t[bold red]атакует:[/] [bold magenta]{attacker['name']}[/]\n\t[bold dark_orange]держит удар:[/] [bold magenta]{defender['name']}[/]\n")
+    #print(f"\n\t[bold red]атакует:[/] [bold magenta]{attacker['name']}[/]\n\t[bold dark_orange]держит удар:[/] [bold magenta]{defender['name']}[/]\n")
     
-    choise = console.input(f"[bold red]{attacker['name']}[/], [bold dark_orange]атаковать героя[/] [bold magenta]{defender['name']}?[/]\n[bold blue]Введите 'да' или 'нет' > [/]")
-    if (choise.lower() == 'да'):
-        print("Идёт атака...")
+    choise = console.input(f"[bold red]{attacker['name']}[/], твоя очередь [bold dark_orange]атаковать героя[/] [bold magenta]{defender['name']}![/]\n[bold blue]Введите 'атака' или 'назад' > [/]")
+    if (choise.lower() == 'атака'):
+        print(f"\t[bold bright_cyan]Идёт атака...[/]")
         time.sleep(1)
         attack_hero(attacker, defender)
         attacker, defender = who_is_attacker(hero_1, hero_2)
+    elif(choise.lower() == 'назад'):
+        print(f"[red]{attacker['name']} совершает тактическое отступление, в надежде вылечиться..[/]")
+        time.sleep(2)
+        health_back_flag = random.randint(0, 1)
+        if (health_back_flag == 1):
+            healme = random.randint(5, 20)
+            print(f"{attacker["name"]} счастливчик, он смог вылечиться [bold green](получено хп: {healme})[/]")
+            attacker["hp"] = attacker["hp"] + healme
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_1['name']}: {hero_1['hp']}[/]")
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_2['name']}: {hero_2['hp']}[/]")
+        elif (health_back_flag == 0):
+            print(f"[red]{attacker['name']} не смог подлатать раны..[/]")
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_1['name']}: {hero_1['hp']}[/]")
+            print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_2['name']}: {hero_2['hp']}[/]")
     elif (choise.lower() == 'выход'):
         print(f"Инициирован выход из игры.")
         exit()
