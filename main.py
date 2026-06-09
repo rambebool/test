c0e708ce9ce12fad7ec77fcf9a4c2c1ca6b90b82
rambebool <rambebool@gmail.com>
2026-06-09 23:48:21 +1000

    update v1

--- main.py
+++ main.py
@@ -6,39 +6,55 @@ from rich.console import Console
 console = Console()
 
 # своя логика
-from models import struct_player, create_hero
+from models import struct_player, create_hero, shop_items
 from combat import who_is_attacker, attack_hero, who_lose
 
 hero_1 = create_hero()
 hero_2 = create_hero()
 attacker, defender = who_is_attacker(hero_1, hero_2)
 
-print(Panel(f"ИМЯ: {attacker['name']}, ХП: {attacker['hp']}, ДАМАГ: {attacker['damage']}, УРОВЕНЬ: {attacker['level']}", title="Герой 1"))
-print(Panel(f"ИМЯ: {defender['name']}, ХП: {defender['hp']}, ДАМАГ: {defender['damage']}, УРОВЕНЬ: {defender['level']}", title="Герой 2"))
+print(Panel(f"ИМЯ: {attacker['name']}, ХП: {attacker['hp']}, ДАМАГ: {attacker['damage']}, УРОВЕНЬ: {attacker['level']}, ЗОЛОТО: {attacker['gold']}", title="Герой 1"))
+print(Panel(f"ИМЯ: {defender['name']}, ХП: {defender['hp']}, ДАМАГ: {defender['damage']}, УРОВЕНЬ: {defender['level']}, ЗОЛОТО: {defender['gold']}", title="Герой 2"))
 
 while(attacker["hp"] > 0 and defender["hp"] > 0):
     #print(f"\n\t[bold red]атакует:[/] [bold magenta]{attacker['name']}[/]\n\t[bold dark_orange]держит удар:[/] [bold magenta]{defender['name']}[/]\n")
     
-    choise = console.input(f"[bold red]{attacker['name']}[/], твоя очередь [bold dark_orange]атаковать героя[/] [bold magenta]{defender['name']}![/]\n[bold blue]Введите 'атака' или 'назад' > [/]")
-    if (choise.lower() == 'атака'):
+    print(f"[bold red]{attacker['name']}[/], твоя очередь [bold dark_orange]атаковать героя[/] [bold magenta]{defender['name']}![/]")
+    choise = console.input(f"[bold blue]Введите 'АТАКА' или 'НАЗАД'! [bold red](или 'МАГАЗ' для доступа в МАГАЗИН): [/]")
+
+    if (choise.lower() == 'магаз'):
+        shop_items(attacker)
+
+    elif (choise.lower() == 'атака'):
         print(f"\t[bold bright_cyan]Идёт атака...[/]")
         time.sleep(1)
         attack_hero(attacker, defender)
         attacker, defender = who_is_attacker(hero_1, hero_2)
+
     elif(choise.lower() == 'назад'):
-        print(f"[red]{attacker['name']} совершает тактическое отступление, в надежде вылечиться..[/]")
+        print(f"[red]{attacker['name']} совершает тактическое отступление, в надежде вылечиться или найти золото...[/]")
         time.sleep(2)
+
         health_back_flag = random.randint(0, 1)
+        gold_back_flag = random.randint(0, 1)
         if (health_back_flag == 1):
             healme = random.randint(5, 20)
-            print(f"{attacker["name"]} счастливчик, он смог вылечиться [bold green](получено хп: {healme})[/]")
+            print(f"{attacker['name']} счастливчик, он смог вылечиться [bold green](получено хп: {healme})[/]")
             attacker["hp"] = attacker["hp"] + healme
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_1['name']}: {hero_1['hp']}[/]")
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_2['name']}: {hero_2['hp']}[/]")
-        elif (health_back_flag == 0):
-            print(f"[red]{attacker['name']} не смог подлатать раны..[/]")
+
+        elif (health_back_flag == 0 or gold_back_flag):
+            print(f"[red]{attacker['name']} не смог подлатать раны или найти золотую монетку..[/]")
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_1['name']}: {hero_1['hp']}[/]")
             print(f"[green]ЗДОРОВЬЕ[/] [bold magenta]{hero_2['name']}: {hero_2['hp']}[/]")
+
+        if (gold_back_flag == 1):
+            give_back_gold = random.randint(1, 2)
+            print(f"{attacker['name']} счастливчик, он нашёл [gold]золото![/] [bold gold](получено золота: {give_back_gold})[/]")
+            attacker["gold"] = attacker["gold"] + give_back_gold
+            print(f"[ЗОЛОТО][/] [bold magenta]{attacker['name']}: {attacker['gold']}[/]")
+
     elif (choise.lower() == 'выход'):
         print(f"Инициирован выход из игры.")
         exit()
--- models.py
+++ models.py
@@ -1,16 +1,19 @@
 import random
+from rich import print
 
-def struct_player(name, hp, damage, level):
+def struct_player(name, hp, damage, level, gold):
     player = {
     "name": name,
     "hp": hp,
     "damage": damage,
-    "level": level
+    "level": level,
+    "gold": gold
 }
     return player
 
 def create_hero():
     name = input(f"Введите ИМЯ героя: ")
+    gold = 5
     level = 0
     damage = random.randint(1, 10)
     while True:
@@ -19,5 +22,45 @@ def create_hero():
         except ValueError:
             print("Ввести можно только числа. Повторите попытку: ")
             continue
-        player = struct_player(name, hp, damage, level)
+        player = struct_player(name, hp, damage, level, gold)
         return player
+    
+def shop_items(hero):
+    print("1. Сапог (+3 к урону) --> [gold]5 золота[/]")
+    print("2. Дубина (+5 к урону) --> [gold]10 золота[/]")
+    print("3. Перцовый баллончик (+10 к урону) --> [gold]20 золота[/]")
+
+    choise = input("Выберите номер товара: ")
+
+    match choise:
+        case "1": 
+            if (hero["gold"] >= 5):
+                hero["gold"] -= 5
+                hero["damage"] = hero["damage"] + 3
+                print(f"[bold white]Вы купили 'САПОГ', ваш текущий урон:[/] [bold red]{hero['damage']}[/]")
+            else:
+                print(f"Недостаточно золота")
+        case "2":
+            if (hero["gold"] >= 10):
+                hero["gold"] = hero["gold"] - 10
+                hero["damage"] = hero["damage"] + 5
+                print(f"[bold white]Вы купили 'ДУБИНУ', ваш текущий урон:[/] [bold red]{hero['damage']}[/]")
+            else:
+                print(f"Недостаточно золота")
+        case "3":
+            if (hero["gold"] >= 20):
+                hero["gold"] = hero["gold"] - 20
+                hero["damage"] = hero["damage"] + 10
+                print(f"[bold white]Вы купили 'ПЕРЦОВЫЙ БАЛЛОНЧИК', ваш текущий урон:[/] [bold red]{hero['damage']}[/]")
+            else:
+                print(f"Недостаточно золота")
+        case "4":
+            print(f"Вы вышли из магазина, ничего не купив.")
+
+        case _:
+            print(f"Такого товара нет.. вы покинули магазин.")
+    return
+    
+
+
+# def random_items()
